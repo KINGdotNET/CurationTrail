789d099013d71b3c7d497d3b47a6ffe3727e1bfc
EM @KING.NET <em@king.net>
2018-07-18 14:43:24 -0400

    Update contact.php
    Update discord channel to https://SteemChat.com URL address.

--- contact.php
+++ contact.php
@@ -3,8 +3,8 @@ require_once('inc/temp/head.php');
 ?>
 <br><br><br>
 <center style="height:100%;">
-<p>You can join our discord channel <a href="https://discord.gg/qhKDfEp">here</a></p>
-<p>Or comment in one of <a href="https://steemit.com/@mahdiyari" target="_blank">@mahdiyari</a>'s posts in steemit.com</p>
+  <p>You can join our discord channel at <a href="https://SteemChat.com"><b>SteemChat.com</b></a></p>
+<p>Or comment in one of <a href="https://steemit.com/@yehey" target="_blank">@Yehey</a>'s posts in steemit.com</p>
 </center>
 
 
